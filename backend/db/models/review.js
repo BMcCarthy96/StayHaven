9cf7d84803389a684a4143dc0db3a076b4fd51bf
Brandon McCarthy <Bmac96.dev@gmail.com>
2024-12-20 19:57:08 -0500

    Fixed review model, remigrated/seeded

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             });
             Review.belongsTo(models.Spot, {
                 foreignKey: "spotId",
-                as: "User",
+                as: "Spot",
             });
             Review.hasMany(models.ReviewImage, {
                 foreignKey: "reviewId",
