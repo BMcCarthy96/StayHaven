5492d0b4e5a366fb00b2cc14dfb4022fd61f341c
Brandon McCarthy <Bmac96.dev@gmail.com>
2024-12-29 12:03:16 -0500

    Modified csrf route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ router.use("/api", apiRouter);
 
 // Add a XSRF-TOKEN cookie
 if (process.env.NODE_ENV !== "production") {
-    router.get("/api/csrf/restore", (req, res) => {
+    router.get("/csrf/restore", (req, res) => {
         const csrfToken = req.csrfToken();
         res.cookie("XSRF-TOKEN", csrfToken);
         res.status(200).json({
