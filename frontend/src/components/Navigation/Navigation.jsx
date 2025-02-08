305c522b0be0848b729455277ea37166a4df1f69
Brandon McCarthy <Bmac96.dev@gmail.com>
2025-02-07 20:55:48 -0500

    Modified css for navbar, added logo, changed title

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,22 +1,24 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import { GiHouse } from "react-icons/gi";
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+    <nav className='navbar'>
+      <div className='icon'>
+        <NavLink to="/" className="home-logo"><GiHouse size={35} color="#000000"/></NavLink>
+        <NavLink to="/" className="title">Stay Haven</NavLink>
+      </div>
+
+      <div className='profile-button'>
       {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-    </ul>
+        <ProfileButton user={sessionUser} />)}
+      </div>
+    </nav>
   );
 }
 
